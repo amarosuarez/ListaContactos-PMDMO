645f56e4788a1889c388159b10ca2d0a8ad33815
amarosuarez <amaro.suarez@iesnervion.es>
2024-10-14 10:44:33 +0200

    Terminado

--- app/src/main/java/com/example/contactosjpc/Login.kt
+++ app/src/main/java/com/example/contactosjpc/Login.kt
@@ -1,47 +1,100 @@
 package com.example.contactosjpc
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.contactosjpc.ui.theme.ContactosJPCTheme
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import java.util.Locale
 
 class Login : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        //enableEdgeToEdge()
         setContent {
-            ContactosJPCTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting2(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+            MaterialTheme {
+                LoginMethod()
             }
         }
     }
 }
 
 @Composable
-fun Greeting2(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun LoginMethod() {
+    var text by rememberSaveable { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    val context= LocalContext.current
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview2() {
-    ContactosJPCTheme {
-        Greeting2("Android")
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Login")
+        TextField(
+            value = text,
+            onValueChange = { text = it },
+            label = { Text("Usuario") }
+        )
+        TextField(
+            value = password,
+            onValueChange = { password = it },
+            label = { Text("Contraseña") },
+            visualTransformation = PasswordVisualTransformation(),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password)
+        )
+        Button(
+            onClick = {
+                val user = "amaro"
+                val pass = "1234"
+                if (text.lowercase(Locale.getDefault()) == user && password == pass) {
+                    val intent = Intent(context, MainActivity::class.java)
+                    context.startActivity(intent)
+                } else {
+                    Toast.makeText(context, "error", Toast.LENGTH_SHORT).show()
+                }
+
+            },
+            modifier = Modifier.padding(16.dp),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color.DarkGray,
+                contentColor = Color.White
+            )
+        ) {
+            // Texto del botón
+            Text(
+                text = "Púlsame",
+                fontSize = 18.sp, // Tamaño de fuente
+                color = Color.White
+            )
+        }
     }
+
+
 }
--- app/src/main/java/com/example/contactosjpc/MainActivity.kt
+++ app/src/main/java/com/example/contactosjpc/MainActivity.kt
@@ -1,105 +1,154 @@
 package com.example.contactosjpc
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.contactosjpc.ui.theme.ContactosJPCTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
         setContent {
-
-        }
-
-
-        /*setContent {
             MaterialTheme {
-                ItemList(
-                    itemContacto = listOf(
-                        Contacto("Laura", "1212332", 1),
-                        Contacto("Maria", "1212332", 1),
-                        Contacto("Amaro", "1212332", 0),
-                        Contacto("Sofia", "1212332", 1),
-                        Contacto("Pedro", "1212332", 0),
-                        Contacto("Lorena", "1212332", 1),
-                        Contacto("Lorena", "1212332", 1),
-                        Contacto("Pablo", "1212332", 0),
-                        Contacto("Pablo", "1212332", 0),
-                    )
-                )
+                ContactList(contacts = listOf(
+                    Contacto("Laura", "1212332", 1),
+                    Contacto("Maria", "1212332", 1),
+                    Contacto("Amaro", "1212332", 0),
+                    Contacto("Sofia", "1212332", 1),
+                    Contacto("Pedro", "1212332", 0),
+                    Contacto("Lorena", "1212332", 1),
+                    Contacto("Lucía", "1212332", 1),
+                    Contacto("Auri", "1212332", 0),
+                    Contacto("Jenri", "1212332", 0),
+                    Contacto("Sebas", "1212332", 0),
+                ))
             }
-        }*/
-    }
-}
-
-@Composable
-fun ItemList(itemContacto: List<Contacto>) {
-    LazyColumn {
-        items(itemContacto) { itemContacto ->
-            ContactoView(contacto = itemContacto)
         }
     }
 }
 
 @Composable
-fun ContactoView(contacto: Contacto) {
-    Card(Modifier.fillMaxWidth()) {
-        Row {
-            Column {
-                Image(
-                    painter = painterResource(id = if (contacto.image == 0) R.drawable.man else R.drawable.women),
-                    contentDescription = "Foto contacto",
-                    Modifier.height(100.dp)
-                )
-            }
-            Column {
-                Text(
-                    text = contacto.nombre,
-                    fontSize = 24.sp,
-                    modifier = Modifier.padding(8.dp)
-                )
-                Text(
-                    text = contacto.telefono,
-                    fontSize = 24.sp,
-                    modifier = Modifier.padding(8.dp)
-                )}}}}
-
-
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun ContactList(contacts: List<Contacto>) {
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(2),
+        contentPadding = PaddingValues(8.dp)
+    ) {
+        items(contacts.size) { index ->
+            VistaContacto(contact = contacts[index])
+        }
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    ContactosJPCTheme {
-        Greeting("Android")
+fun VistaContacto(contact: Contacto) {
+    // Variable que almacena si se estan mostrando los detalles
+    var showDetails by remember { mutableStateOf(false) }
+
+    // Variable que obtiene las iniciales
+    val initial: String = contact.nombre.substring(0, 1)
+
+    // Contexto
+    val context = LocalContext.current
+
+    Card(
+        shape = RectangleShape,
+        modifier = Modifier
+            .padding(8.dp)
+            .fillMaxWidth()
+            .clickable { showDetails = !showDetails },
+        colors = CardDefaults.cardColors(
+            containerColor = Color(0xFFFF9E9E)
+        )
+    ) {
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Image(
+                painter = painterResource(
+                    // Si la imagen es igual a 1 es mujer
+                    if (contact.image == 1) R.drawable.women else R.drawable.man
+                ),
+                contentDescription = "Imagen según el género",
+                modifier = Modifier
+                    .size(36.dp)
+                    .padding(top = 5.dp),
+                contentScale = ContentScale.Crop,
+            )
+
+            // Separador
+            Spacer(modifier = Modifier.height(8.dp))
+
+            AnimatedVisibility(
+                visible = !showDetails,
+                enter = fadeIn(),
+                exit = fadeOut()
+            ) {
+                Text(
+                    text = initial,
+                    fontSize = 32.sp,
+                    fontWeight = FontWeight.Bold
+                )
+            }
+
+            AnimatedVisibility(
+                visible = showDetails,
+                enter = fadeIn(),
+                exit = fadeOut()
+            ) {
+                Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                    Text(text = contact.nombre, fontSize = 20.sp, fontWeight = FontWeight.Bold)
+                    Spacer(modifier = Modifier.height(4.dp))
+                    Text(
+                        text = contact.telefono,
+                        color = Color.Blue,
+                        modifier = Modifier.clickable {
+                            val intent = Intent(Intent.ACTION_DIAL).apply {
+                                data = Uri.parse("tel:${contact.telefono}")
+                            }
+                            context.startActivity(intent)
+                        }
+                    )
+                }
+            }
+        }
     }
 }
